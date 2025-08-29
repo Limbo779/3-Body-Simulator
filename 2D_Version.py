14eef696c6f2489428d7362e08683a4b19dbaa4e
Limbo779 <sarveshvr799@zohomail.in>
2025-08-29 18:14:24 +0530

    Made Collision Function

--- 2D_Version.py
+++ 2D_Version.py
@@ -13,8 +13,25 @@ v2=np.array([0,0])
 
 # body 3
 p3=np.array([3,4])
-m1=1
-v1=np.array([0,0])
+m3=1
+v3=np.array([0,0])
 
 # G Const
 G=6.67430*(10**(-11))
+
+# managing collision
+def collision(x):
+    global v1,v2,v3
+    # a means v1 and v2
+    # b means v1 and v3
+    # c means v2 and v3
+    if x == "a":
+        v1=v1*(-1)
+        v2=v2*(-1)   
+    elif x == "b":
+        v1=v1*(-1)
+        v3=v3*(-1)
+    else:
+        v3=v3*(-1)
+        v2=v2*(-1)
+
