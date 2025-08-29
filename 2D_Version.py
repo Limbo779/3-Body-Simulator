db0a765a5731f9d252a8320e07967cfc1f16b181
Limbo779 <sarveshvr799@zohomail.in>
2025-08-29 17:44:54 +0530

    Initialize model 1 for 2D

--- 2D_Version.py
+++ 2D_Version.py
@@ -1,1 +1,17 @@
 import numpy as np
+from matplotlib import pyplot as plt
+
+# body 1
+p1=np.array([1,1])
+m1=1
+v1=np.array([0,0])
+
+# body 2
+p2=np.array([5,1])
+m2=1
+v2=np.array([0,0])
+
+# body 3
+p3=np.array([3,4])
+m1=1
+v1=np.array([0,0])
