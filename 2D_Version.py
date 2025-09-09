dfd51f6a41ae1fd9c5a61c873907e0f765dcaba7
Limbo <sarveshvr799@zohomail.in>
2025-09-09 16:54:28 +0530

    Neat Animation for 2D_Version
    Made a Nice and usable animation

--- 2D_Version.py
+++ 2D_Version.py
@@ -1,28 +1,87 @@
 import numpy as np
-from matplotlib import pyplot as plt
+import matplotlib
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# We'll use a class to maintain state across calls
+class Animator:
+    def __init__(self):
+        self.x_list = []  # To collect x coordinates over time
+        self.y_list = []  # To collect y coordinates over time
+        self.is_started = False  # Flag to prevent multiple animations
+
+    def animate(self, x=None, y=None, command=None):
+        if command == 'Start':
+            if self.is_started or not self.x_list or not self.y_list:
+                print("Animation already started or no data collected.")
+                return
+            self.is_started = True
+            
+            # Create figure and axis
+            fig, ax = plt.subplots()
+            
+            # Set axis limits with padding
+            all_x = [xi for sublist in self.x_list for xi in sublist]
+            all_y = [yi for sublist in self.y_list for yi in sublist]
+            ax.set_xlim(min(all_x) - 1, max(all_x) + 1)
+            ax.set_ylim(min(all_y) - 1, max(all_y) + 1)
+            ax.set_aspect('equal')
+            ax.set_title('3-Body Simulation Animation')
+            
+            # Initialize scatters for three bodies with different colors
+            colors = ['red', 'blue', 'green']
+            scatters = [ax.plot([], [], 'o', color=colors[i], markersize=8)[0] for i in range(3)]
+            
+            # Update function for animation
+            def update(frame):
+                for i in range(3):
+                    scatters[i].set_data(self.x_list[frame][i], self.y_list[frame][i])
+                return scatters
+            
+            # Create and show animation
+            ani = animation.FuncAnimation(fig, update, frames=len(self.x_list), interval=50, blit=True)
+            plt.show()
+        
+        else:
+            # Collect data inside the loop (assume x and y are lists like [x1, x2, x3])
+            if x is not None and y is not None:
+                self.x_list.append(x)
+                self.y_list.append(y)
+            else:
+                print("Please provide x and y lists when calling inside the loop.")
+
+# Usage:
+# Create an instance
+animator = Animator()
+
+# Inside your simulation loop:
+# animator.animate([current_x1, current_x2, current_x3], [current_y1, current_y2, current_y3])
+
+# After the loop:
+# animator.animate(command='Start')
 
 def mod(x): # returns the modular of a vector
     return (np.dot(x,x))**(0.5)
 
 # body 1
-p1=np.array([1,1])
+p1=np.random.randint(0,10,size=2)
 m1=10
 v1=np.array([0,0])
 
 # body 2
-p2=np.array([5,1])
+p2=np.random.randint(0,10,size=2)
 m2=10
 v2=np.array([0,0])
 
 # body 3
-p3=np.array([3,4])
+p3=np.random.randint(0,10,size=2)
 m3=10
 v3=np.array([0,0])
 
 # G Const
 G=1 #6.67430*(10**(-11))
 # small timestep
-dt=0.1
+dt=0.01
 
 # managing collision (when two bodies touch each they make elastic collision)
 # they touch each other when they are below the distance of 2 btw each other (1 is their radius)
@@ -41,15 +100,32 @@ def collision(x):
         v3=v3*(-1)
         v2=v2*(-1)
 
-for _ in range(1000):
-    #plt.clf()
-    ra=(p1-p2)*-1 # vector btw body 1 and body 2 
-    rb=(p1-p3)*-1 # vector btw body 1 and body 3
-    rc=(p2-p3)*-1 # vector btw body 2 and body 3
+#fig = plt.figure() 
+#l,=plt.plot([],[])
+#
+#plt.xlim(-100,100)
+#plt.ylim(-100,100)
 
+
+
+#metadata = dict(title='Movie',artist='Limbo')
+#writer=PillowWriter(fps=5,metadata=metadata)
+#
+#with writer.saving(fig,"body.gif",72):    
+for _ in range(3000):
+    #plt.clf()
+    ra=(p2-p1) # vector btw body 1 and body 2 
+    rb=(p3-p1) # vector btw body 1 and body 3
+    rc=(p3-p2) # vector btw body 2 and body 3
+    if mod(p1-p2) < 0.5 :
+        collision("a")
+    elif mod(p1-p3) < 0.5 :
+        collision("b")
+    elif mod(p3-p2) < 0.5 :
+        collision("c")
     fa=(G*m1*m2*ra)/(mod(ra)**3) # force between body 1 and body 2
-    fb=(G*m1*m2*rb)/(mod(rb)**3) # force between body 1 and body 3
-    fc=(G*m1*m2*rc)/(mod(rc)**3) # force between body 2 and body 3
+    fb=(G*m1*m3*rb)/(mod(rb)**3) # force between body 1 and body 3
+    fc=(G*m3*m2*rc)/(mod(rc)**3) # force between body 2 and body 3
     
     # position and velocity updating
     
@@ -69,15 +145,7 @@ for _ in range(1000):
     x=[p1[0],p2[0],p3[0]]
     y=[p1[1],p2[1],p3[1]]
     
+    animator.animate(x, y)
 
-    #plt.scatter(*p1)
-    #plt.scatter(*p2)
-    #plt.scatter(*p3)
+animator.animate(command='Start')    
 
-    plt.scatter(x,y)
-    plt.pause(0.1)
-    plt.close()
-
-
-
-plt.show()
